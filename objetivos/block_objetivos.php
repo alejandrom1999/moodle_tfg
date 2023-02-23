149b5ec7ae87bf15ebe707c7db467f75584f729f
alexgonzaalez <alejandromanuel.gonzalez@mail.polimi.it>
2023-02-23 02:05:35 +0100

    Creacion de porcentajes y rellenando con datos de ejemplo la BBDD

--- objetivos/block_objetivos.php
+++ objetivos/block_objetivos.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once("$CFG->libdir/formslib.php");
 
 class block_objetivos extends block_base {
 
@@ -28,7 +27,60 @@ class block_objetivos extends block_base {
         $this->content         =  new stdClass;
         $this->content->text   = '';
 
+        function porcentaje_objetivo_usuario($objetivo_nombre, $usuario_id)
+        {
 
+            $tareas_hechas = 0;
+            $id_objetivo = get_id_objetivo($objetivo_nombre);
+            $numero_tareas_objetivo = numero_tareas($id_objetivo);
+            $tareas = tarea_objetivo_n($objetivo_nombre);
+
+            foreach ($tareas as $t)
+            {
+                if(get_status_tarea_usuario($usuario_id, $t) == 1)
+                {
+                    $tareas_hechas++;
+                }
+
+            }
+
+            return round(($tareas_hechas/$numero_tareas_objetivo) * 100,2);;
+        }
+
+        function porcentaje_curso_usuario($usuario_id)
+        {
+            global $DB;
+
+            global $DB, $COURSE;
+            $sql1 = "SELECT b_o.nombre
+                 FROM {objetivo} b_o
+                 WHERE b_o.id_course = $COURSE->id";
+
+            $nombres = $DB->get_records_sql($sql1);
+            $num_objetivos = 0;
+            $porcentaje_total = 0;
+            foreach ($nombres as $n)
+            {
+                $num_objetivos++;
+                $porcentaje_total += porcentaje_objetivo_usuario($n->nombre,$usuario_id);
+
+            }
+            return round($num_objetivos/$porcentaje_total,2);;
+        }
+
+
+        /* Numero de tareas de un objetivo */
+        function numero_tareas($objetivo_id)
+        {
+            global $DB;
+            $numero = $DB->count_records_sql("
+                    SELECT COUNT(*) 
+                    FROM {tarea} t 
+                    WHERE t.id_objetivo = $objetivo_id");
+
+            return $numero;
+         }
+        /*Lista de objetivos del curso*/
         function lista_objetivos()
         {
             global $DB, $COURSE;
@@ -51,7 +103,7 @@ class block_objetivos extends block_base {
 
             return $objetivos;
         }
-
+        /*Crear un objetivo a partir de un nombre*/
         function crear_objetivo($nombre) : void
         {
             global $COURSE, $DB;
@@ -62,6 +114,7 @@ class block_objetivos extends block_base {
             $DB->insert_record(objetivo , $objetivo_n );
         }
 
+        /*Crear una tarea a partir de una id_objetivo, id_tarea y nombre de la tarea*/
         function crear_tarea($id_objetivo, $id_tarea, $nombre) : void
         {
             global $COURSE, $DB;
@@ -73,6 +126,7 @@ class block_objetivos extends block_base {
             $DB->insert_record(tarea, $tarea_n);
 
         }
+        /*Obtener el id de un objetivo a partir de su nombre*/
         function get_id_objetivo($nombre_obj): string {
             global $DB, $COURSE;
             $sql = " SELECT *
@@ -90,78 +144,75 @@ class block_objetivos extends block_base {
             return $sal1;
         }
 
-        function tarea_objetivo_n($name) : string
+        /*TAREAS DE UN OBJETIVO EN CONCRETO*/
+        function tarea_objetivo_n($nombre_objetivo)
         {
-
            global $DB;
 
-           $id = get_id_objetivo($name);
+           $id = get_id_objetivo($nombre_objetivo);
 
            $sql2 = "SELECT t.nombre
                  FROM {tarea} t
                  WHERE t.id_objetivo = $id";
-            $sal2 = '';
-            $sal2_2 = $DB->get_records_sql($sql2);
-            foreach ($sal2_2 as $obj_n)
-            {
-                $sal2 .= $obj_n->nombre . ',';
+
+            $tareas = $DB->get_records_sql($sql2);
+
+            $arr_tarea = array();
+            $i = 0;
+
+            foreach($tareas as $nom) {
+                $arr_tarea[$i++] = $nom->nombre;
             }
 
-            return $sal2;
+            return $arr_tarea;
 
         }
-
-        function get_id_actividad($name) : int
+        /*FUNCION QUE DEVUELVE LA ID DE UNA ACTIVIDAD EN BASE A SU NOMBRE*/
+        function get_id_actividad($nombre_actividad)
         {
             global $DB;
-            $sql_2 = "SELECT *
-                      FROM {assign} a
-                      WHERE a.name = $name";
 
-            $assign_id = '';
-            $sal2_2 = $DB->get_records_sql($sql_2);
-            foreach ($sal2_2 as $obj_n)
+            $records_assign = $DB->get_records('assign');
+            foreach ($records_assign as $record)
             {
-                if($obj_n->name === $name){
-                    $assign_id = $obj_n->id;
+                if($record->name === $nombre_actividad)
+                {
+                    $sal1 = $record->id;
                 }
             }
-            return $assign_id;
+
+            return $sal1;
         }
 
-        /*Se devuelve true o false segun se ha completado o no esa tarea ( si se ha entregado ). */
-        function get_status_tarea_usuario($user, $assignment_name): string
+        /*  FUNCION QUE DEVUELVE LA COMPROBACION ( 0 = FALSE, 1 = TRUE) SI SE HA ENTREGADO UNA
+            TAREA POR UN USUARIO Y HA SIDO APROBADA
+            A PARTIR DEL NOMBRE DE ESEA TAREA.
+
+            $user = user_id.
+            $assignment_name = nombre de la tarea.
+
+        */
+        function get_status_tarea_usuario($user, $assignment_name)
         {
             global $DB;
 
             $id_tar = get_id_actividad($assignment_name);
 
             $sql = "SELECT * 
-                    FROM {assign_submission} as_s
+                    FROM {assign_grades} as_s
                     WHERE as_s.userid = $user 
                     AND
-                    as_s.assignment = $id_tar";
+                    as_s.assignment = $id_tar
+                    AND
+                    as_s.grade >= 50 ";
 
-
-
-            $exist = $DB->record_exists_sql($sql);
-
-
-            return $exist;
+            return $DB->record_exists_sql($sql);;
         }
 
         $objetivos = lista_objetivos();
 
-
-        $nombres_tareas = tarea_objetivo_n('Superar a todos mis compañeros');
-
-
-
         $data['objetivos'] = $objetivos;
-
-
-
-
+        $data['porcentaje'] = porcentaje_objetivo_usuario('Superar a todos mis compañeros', 3);
 
 
         $this->content->text .= $OUTPUT->render_from_template($templatename, $data);
